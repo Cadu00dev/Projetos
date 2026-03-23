ac89bdc594e43ad9e2cba6de0c7e43fae4024751
Carlos Eduardo <cesr00904@gmail.com>
2026-03-23 15:42:02 -0300

    primeiro commit

---
+++ projeto.py
@@ -0,0 +1,12 @@
+print("Bem vindo a sua agenda de tarefas")
+
+acao = input("Deseja adicionar atividades de hoje? presione atv \n"
+             "Deseja ver o total de atividades? presione enter \n")
+
+if acao == "atv":
+    atividades = input("Quais são suas atividade?\n")
+    listaatv = atividades.split(",")
+    quantidade = len(listaatv)
+    print(f"Voce tem", quantidade,"atividades, sendo elas:", listaatv)
+else:
+    print("Voce nao tem atividades cadastradas.")
